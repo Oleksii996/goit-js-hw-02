2de1261074ab1af996c9353b81a13869be336bc8
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-09-21 14:28:00 +0200

    update code

--- js/task-1.js
+++ js/task-1.js
@@ -8,28 +8,3 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
-// task-2
-
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
-
-console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-// task-3
-
-function getElementWidth(content, padding, border) {
-  const a = parseFloat(content); // Так я перезаписав змінні (parseFloat краще, бо не ціле число)
-  const b = parseFloat(padding); //
-  const c = parseFloat(border); //
-
-  return a + b * 2 + c * 2; // Підставив нові змінні
-}
-
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
