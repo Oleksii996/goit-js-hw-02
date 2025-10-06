813ae31ed9086ba172a535eefdea76573bbbe704
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-10-06 20:09:18 +0200

    update code

--- js/task-1.js
+++ js/task-1.js
@@ -9,12 +9,14 @@
 // Доповни функцію таким чином:
 // Оголоси змінну для зберігання загальної суми замовлення (загальна вартість усіх замовлених дроїдів) і задай їй вираз розрахунку цієї суми. Додай перевірку, чи зможе клієнт оплатити замовлення: якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, функція має повертати рядок "Insufficient funds!" в іншому випадку функція має повертати рядок "You ordered <quantity> droids worth <totalPrice> credits!", де <quantity> це кількість замовлених дроїдів, а <totalPrice> це їх загальна вартість.
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
+{
+  function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const totalPrice = quantity * pricePerDroid;
 
-  if (totalPrice > customerCredits) {
-    return 'Insufficient funds!';
-  } else return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    if (totalPrice > customerCredits) {
+      return 'Insufficient funds!';
+    } else return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,19 @@
 // task-2
+
 // Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
 
 // Доповни код функції таким чином, що:
 // Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
 // Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
 
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  }
-
-  else {
-    (message = message.slice(0, maxLength) + '...');
-    return message;
+{
+  function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+      return message;
+    } else {
+      message = message.slice(0, maxLength) + '...';
+      return message;
+    }
   }
 }
 
